d3b6baac30ec963eeca42b736970f0be47c525aa
Denverjack Lagasca <83049116+deejay1987@users.noreply.github.com>
2022-03-11 10:32:04 +0800

    update codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
         .on("click", "#generate_profile_btn", function(){
 
             /* This will allow selected options value to be generated */
-            for(let i = 0; i < $("#number_generate_profile").val(); i++){
+            let max_generated_profile = $("#number_generate_profile").val();
+
+            for(let index = 0;  index < max_generated_profile; index++){
                 let profile_id = Math.floor((Math.random() * 100) + 1);
                 
                 $.ajax({
@@ -12,32 +14,30 @@ $(document).ready(function() {
                     dataType: 'json',
                     success: function(data) {
                         /* this will generate random profile */
-                        const profile_details = data.results[0];
-
-                        $("#generated_profile").append(
-                            "<li data-profile-id='" + profile_id + "' class='profile'>" +
-                            `<img src="`+ profile_details.picture.large +`" alt="Random User">` +
-                                `<div class="profile_details">
-                                    <h2>` + profile_details.name.first + " " + profile_details.name.last +`</h2>
-                                    <span><i class="fa fa-envelope"></i>`+ " " + profile_details.email +`</span>
-                                    <p><i class="fa fa-location-dot"></i>`+ " " + profile_details.location.street.number + " "
-                                        + profile_details.location.street.name + " "
-                                        + profile_details.location.state + " "
-                                        + profile_details.location.country + 
-                                    `</p>
-                                </div>` +
-                                "<button type='button' class='close_profile_btn'>x</button>" +
-                            "</li>"
-                        );
+                        let profile_details = data.results[0];
+                        let address = profile_details.location;
+                        let clone_profile = $(".profile_clone").clone();
+                        /* Process profile clone */
+                        clone_profile.removeClass("hidden profile_clone")
+                                    .attr("data-profile-id", + profile_id)
+                                                
+                        clone_profile.find(".profile_img").attr("src", profile_details.picture.large );
+                        clone_profile.find(".full_name").text(profile_details.name.first + " " + profile_details.name.last);
+                        clone_profile.find(".email_address_text").text(profile_details.email);
+                        clone_profile.find(".address_text").text(address.state + " " + address.country);
+                        /* Append the profile clone */
+                        $("#generated_profile").append(clone_profile);
                     }
                 });
             };
         }) 
         /* This will delete the specific profile */
-        .on("click", ".close_profile_btn", function() {
+        .on("click", ".remove_profile_btn", function() {
             
-            $("#generated_profile li[data-profile-id="+ $(this).closest(".profile").data("profile-id") +"]").remove();
+            $(this).closest("li").remove();
+            // $("#generated_profile li[data-profile-id="+ $(this).closest(".profile").data("profile-id") +"]").remove();
         })
+        /* Reload page */
         .on("click", "#reset_btn", function(){
             window.location.reload();
         });
